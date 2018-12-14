8ce88ad3df1e4f40f4c7efb6009755b234771e79
SI <stennly.mkiv@gmail.com>
2018-12-14 12:58:36 +0000

    Replaced stdin with open(); added timing code

--- 1.py
+++ 1.py
@@ -1,24 +1,27 @@
-#!/bin/python2
+import time
 
-import sys
-
-# 1.1
-# print sum([ int(line.strip()) if line.strip() else 0 for line in sys.stdin ])
+def part1():
+    return sum([int(line) for line in open('1.in')])   
 
 # 1.2
-found = 0
-total = 0
-seen = {0 : 1}
-freqs = [line.strip() for line in sys.stdin]
-while(not(found)):
-    for line in freqs:
-        if line.strip():
-            total += int(line.strip())
-            if total in seen:
-                print "Seen again:", total
-                found = 1
-                break
-            else:
-                seen[total] = 1
-if not(found):
-    print "No repeats"
+#found = 0
+#total = 0
+#seen = {0 : 1}
+#freqs = [line.strip() for line in sys.stdin]
+#while(not(found)):
+#    for line in freqs:
+#        if line.strip():
+#            total += int(line.strip())
+#            if total in seen:
+#                print("Seen again:", total)
+#                found = 1
+#                break
+#            else:
+#                seen[total] = 1
+#if not(found):
+#    print("No repeats")
+t0 = time.time()
+print(part1())
+t1 = time.time()
+print(t1 - t0)
+
