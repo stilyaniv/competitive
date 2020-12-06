863977e16462990cb4530d776ebb0cf5dd1827d5
stilyaniv <stennly.mkiv@gmail.com>
2020-12-06 20:12:19 +0000

    2020 day 5 solutions

---
+++ 2020/day_5.py
@@ -0,0 +1,49 @@
+def get_row_id(string):
+    return int(string.replace("F", "0").replace("B", "1"), 2)
+
+
+def get_col_id(string):
+    return int(string.replace("L", "0").replace("R", "1"), 2)
+
+
+def get_seat_info(string):
+    row_id = get_row_id(string[:7])
+    col_id = get_col_id(string[7:])
+
+    return row_id, col_id, row_id * 8 + col_id
+
+
+def get_max_id_from_file():
+    max_id = 0
+    with open("day_5_input.txt") as file:
+        for line in file:
+            _, _, seat_id = get_seat_info(line.strip())
+            if seat_id > max_id:
+                max_id = seat_id
+    return max_id
+
+
+def insert_sorted(new, items):
+    for idx in range(len(items)):
+        if new <= items[idx]:
+            items.insert(idx, new)
+            return
+    items.append(new)
+
+
+def get_missing_id_from_file():
+    with open("day_5_input.txt") as file:
+        ids = []
+        for line in file:
+            _, _, seat_id = get_seat_info(line.strip())
+            insert_sorted(seat_id, ids)
+    for idx in range(1,len(ids)-1):
+        if ids[idx+1] - ids[idx] > 1:
+            return ids[idx] + 1
+        if ids[idx] - ids[idx-1] > 1:
+            return ids[idx] - 1
+
+
+if __name__ == "__main__":
+    print(get_max_id_from_file())
+    print(get_missing_id_from_file())
---
+++ 2020/test_day_5.py
@@ -0,0 +1,13 @@
+import day_5
+
+def test_get_seat_info():
+    assert (70, 7, 567) == day_5.get_seat_info("BFFFBBFRRR")
+    assert (14, 7, 119) == day_5.get_seat_info("FFFBBBFRRR")
+    assert (102, 4, 820) == day_5.get_seat_info("BBFFBBFRLL")
+
+def test_get_missing_id_from_file():
+    assert 615 == day_5.get_missing_id_from_file()
+
+
+def test_get_max_id_from_file():
+    assert 953 == day_5.get_max_id_from_file()
