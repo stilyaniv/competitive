15786706fe2cc55cbdd3a8349df9d58df352e618
stilyaniv <stennly.mkiv@gmail.com>
2020-12-07 02:47:44 +0000

    Lint fixes

--- 2020/day_4.py
+++ 2020/day_4.py
@@ -1,42 +1,4 @@
-from functools import partial
-import re
-
-
-def validate_length(lengthStr):
-    r = re.compile(r"^(\d{0,1}\d\d)(in|cm)$")
-    match = r.match(lengthStr)
-    if match:
-        num, unit = match.groups()
-        if unit == "cm":
-            return 150 <= int(num) <= 193
-        elif unit == "in":
-            return 59 <= int(num) <= 76
-        return False
-
-
-def validate_hex_color(colorStr):
-    r = re.compile("^#[0-9a-f]{6}$")
-    return r.match(colorStr) is not None
-
-
-def validate_num_within_range(start, end, numStr):
-    try:
-        num = int(numStr)
-        return start <= num <= end
-    except ValueError:
-        return False
-
-
-def validate_eye_color(colorStr):
-    return colorStr in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
-
-
-def validate_passport_id(string):
-    r = re.compile("^[0-9]{9}$")
-    return r.match(string) is not None
-
-
-"""    
+"""
     byr (Birth Year) - four digits; at least 1920 and at most 2002.
     iyr (Issue Year) - four digits; at least 2010 and at most 2020.
     eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
@@ -48,6 +10,44 @@ def validate_passport_id(string):
     pid (Passport ID) - a nine-digit number, including leading zeroes.
     cid (Country ID) - ignored, missing or not.
 """
+
+from functools import partial
+import re
+
+
+def validate_length(length_str):
+    regex = re.compile(r"^(\d{0,1}\d\d)(in|cm)$")
+    match = regex.match(length_str)
+    if match:
+        num, unit = match.groups()
+        if unit == "cm":
+            return 150 <= int(num) <= 193
+        elif unit == "in":
+            return 59 <= int(num) <= 76
+        return False
+
+
+def validate_hex_color(color_str):
+    regex = re.compile("^#[0-9a-f]{6}$")
+    return regex.match(color_str) is not None
+
+
+def validate_num_within_range(start, end, num_str):
+    try:
+        num = int(num_str)
+        return start <= num <= end
+    except ValueError:
+        return False
+
+
+def validate_eye_color(color_str):
+    return color_str in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+
+
+def validate_passport_id(string):
+    regex = re.compile("^[0-9]{9}$")
+    return regex.match(string) is not None
+
 REQUIRED_FIELDS = {
     "byr": partial(validate_num_within_range, 1920, 2002),
     "iyr": partial(validate_num_within_range, 2010, 2020),
@@ -59,22 +59,22 @@ REQUIRED_FIELDS = {
 }
 
 
-def get_fields_from_line(lineFields):
-    currentFields = {}
-    for field in lineFields:
-        currentFields.update(dict([field.split(":")]))
-    return currentFields
+def get_fields_from_line(line_fields):
+    current_fields = {}
+    for field in line_fields:
+        current_fields.update(dict([field.split(":")]))
+    return current_fields
 
 
-def get_passport_fields(passportStr):
-    fieldsValues = {}
-    for pair in passportStr.split():
-        fieldsValues.update(dict([pair.split(":")]))
-    return fieldsValues
+def get_passport_fields(passport_str):
+    field_values = {}
+    for pair in passport_str.split():
+        field_values.update(dict([pair.split(":")]))
+    return field_values
 
 
-def validate_passport(passportStr):
-    fields = get_passport_fields(passportStr)
+def validate_passport(passport_str):
+    fields = get_passport_fields(passport_str)
 
     if not set(REQUIRED_FIELDS).issubset(fields):
         return False
@@ -85,20 +85,23 @@ def validate_passport(passportStr):
             return False
     return True
 
-
-if __name__ == "__main__":
+def count_valid_passports_from_file():
     count = 0
     with open("day_4_input.txt") as file:
-        currentPassport = ""
+        current_passport = ""
         for line in file:
             if len(line.split()) == 0:
-                count += validate_passport(currentPassport)
-                currentPassport = ""
+                #TODO add optional flag to perform shallow or deep check
+                count += validate_passport(current_passport)
+                current_passport = ""
                 continue
-            currentPassport += line
-        #end of file is ignored if there aren't new lines, 
+            current_passport += line
+        #end of file is ignored if there aren't new lines,
         # so last line and passport needs to be validated after EoF
-        currentPassport += line
-        count += validate_passport(currentPassport)
+        current_passport += line
+        count += validate_passport(current_passport)
+    return count
 
-    print(count)
+
+if __name__ == "__main__":
+    print(count_valid_passports_from_file())
