889dfbc360853164173064f0f7949ee8ac8a9ab2
stilyaniv <stennly.mkiv@gmail.com>
2024-12-01 17:49:41 +0000

    aoc 2022 day 1 simpler solution

--- src/aoc_2022/day1.py
+++ src/aoc_2022/day1.py
@@ -6,23 +6,18 @@ def sum_top_n_stacks(n=1):
             new_top_stacks = []
             is_new_inserted = False
             i = 0
-            while not is_new_inserted:
-                if i >= len(top_stacks):
+            while i < len(top_stacks):
+                top_stack_i = top_stacks[i]
+                if not is_new_inserted and current_stack >= top_stack_i:
                     new_top_stacks.append(current_stack)
                     is_new_inserted = True
                 else:
-                    top_stack_i = top_stacks[i]
-                    if current_stack >= top_stack_i:
-                        new_top_stacks.append(current_stack)
-                        is_new_inserted = True
-                    else:
-                        new_top_stacks.append(top_stack_i)
-                        i += 1
-            current_stack = 0
-            while i < len(top_stacks):
-                new_top_stacks.append(top_stacks[i])
-                i += 1
+                    new_top_stacks.append(top_stack_i)
+                    i += 1
+            if not is_new_inserted:
+                new_top_stacks.append(current_stack)
             top_stacks = new_top_stacks
+            current_stack = 0
         else:
             current_stack += int(line)
     print(top_stacks)
