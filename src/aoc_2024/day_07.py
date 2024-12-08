0dcc68ee891e50e169525fdc9b0802f5f854eeae
stilyaniv <stennly.mkiv@gmail.com>
2024-12-08 18:07:24 +0000

    restored day 7 part 1 and refactored

--- src/aoc_2024/day_07.py
+++ src/aoc_2024/day_07.py
@@ -1,9 +1,9 @@
 """
-https://adventofcode.com/
-"""  # TODO link to problem
+https://adventofcode.com/2024/day/7
+"""
+
 import io
 from pathlib import Path
-from pprint import pprint
 
 EXAMPLE = """\
 190: 10 19
@@ -17,6 +17,10 @@ EXAMPLE = """\
 292: 11 6 16 20
 """
 
+PART1_EXAMPLE_OUTPUT = 3749
+PART2_EXAMPLE_OUTPUT = 11387
+PART1_OUTPUT = 303876485655
+PART2_OUTPUT = 146111650210682
 
 INPUT_FILE_PATH = f"{Path(__file__).parent / Path(__file__).stem}.txt"
 
@@ -24,148 +28,42 @@ INPUT_FILE_PATH = f"{Path(__file__).parent / Path(__file__).stem}.txt"
 def comb1(head, tail, total):
     if len(head) == 1:
         head = head[0]
-        return (
-            head * tail == total
-            or head + tail == total
-            or int(str(head) + str(tail))
-            == total  # TODO restore comb1 to the part 1 solution
-        )
+        return head * tail == total or head + tail == total
     else:
-        splits = []
-        for i in range(1, len(str(total))):
-            split = [int(str(total)[:i]), int(str(total)[i:])]
-            splits.extend(split)
-
-        return (
-            comb1(head[:-1], head[-1], total - tail)
-            or comb1(head[:-1], head[-1], total / tail)
-            or any(
-                [comb1(head[:-1], head[-1], t) for t in splits]
-            )  # TODO restore comb1 to the part 1 solution
+        return comb1(head[:-1], head[-1], total - tail) or comb1(
+            head[:-1], head[-1], total / tail
         )
 
 
 def part_1(file):
     ans = 0
     for line in file:
-        # print(total, line)
         total, els = line.strip().split(":")
         els = [int(e) for e in els.strip().split()]
         total = int(total)
-        print(total, els)
         if comb1(els[:-1], els[-1], total):
             ans += total
     return ans
 
 
-"""
-    part 2
-    ------
-    190: 10 19 has only one position that accepts an operator: between 10 and 19. Choosing + would give 29, but choosing * would give the test value (10 * 19 = 190).
-    3267: 81 40 27 has two positions for operators. Of the four possible configurations of the operators, two cause the right side to match the test value: 
-            81 + 40 * 27 and 81 * 40 + 27 both equal 3267 (when evaluated left-to-right)!
-    156: 15 6 can be made true through a single concatenation: 15 || 6 = 156.
-    192: 17 8 14 can be made true using 17 || 8 + 14.
-    292: 11 6 16 20 can be solved in exactly one way: 11 + 6 * 16 + 20.
-
-    7290: 6 8 6 15 can be made true using 6 * 8 || 6 * 15.
-"""
-
-# EXAMPLE = """\
-# 190: 10 19
-# 3267: 81 40 27
-# 83: 17 5
-# 156: 15 6
-# 7290: 6 8 6 15
-# 161011: 16 10 13
-# 192: 17 8 14
-# 21037: 9 7 18 13
-# 292: 11 6 16 20
-# """
-
-# EXAMPLE = """6815: 6 8 15"""
-# EXAMPLE = """161011: 16 10 13"""
-
-# 7290: 6 8 6 15 can be made true using 6 * 8 || 6 * 15.
-#  48 || 6 * 15
-#    486   * 15
-# EXAMPLE = """7290: 6 8 6 15"""
-
-"""
-head    tail    total
-[6 8 6] 15      7290
-.[6 8 6] * 15    7290               !!!
-..[6 8] 6         486 = 7290 // 15  
-...[6 8] * 6       486              !!!
-....6 8             81 = 486 // 6
-....6 + 8           14
-....6 * 8           48
-....6 || 8          68
-...[6 8] + 6       486
-....6 8             480 = 486 - 6
-....6 + 8           14
-....6 * 8           48
-....6 || 8          68
-...[6 8] || 6      486              !!!
-....6 8             48 = 48 << 6     
-....6 + 8           14
-....6 * 8           48              !!!
-....6 || 8          68
-.[6 8 6] + 15      7290
-.[6 8 6] || 15      7290
-
-"""
-
-
-def comb2_reduction(head, tail, total):
-    """
-    from itertools import permutations
-    import pprint
-    from pprint import pprint
-        doesn't work due to:
-        7290: 6 8 6 15
-        causing a floating point result for integer division
-        even after fixing that it's tricky due to floating point .0
-    """
-    print(head, tail, total)
+def comb2(head, tail, total):
+    print(f"{head} _ {tail} == {total}?")
     if len(head) == 1:
-        # print(head, tail)
         head = head[0]
+
         return (
             head * tail == total
             or head + tail == total
             or int(str(head) + str(tail)) == total
         )
     else:
-        # copy_total = total
-        # total_digits = []
-        # while copy_total // 10 != 0:
-        #     total_digits = [copy_total % 10] + total_digits
-        #     copy_total = copy_total // 10
-        # total_digits = [copy_total] + total_digits
-
-        # total_digits = str(total)
-        # splits = []
-        # for i in range(1, len(total_digits)):
-        #     split = [total_digits[:i], total_digits[i:]]
-        #     splits.extend(split)
-
-        # print(head[:-1], head[-1], total)
-        # start_next = len(str(tail))
-
-        total = int(total)
         return (
-            comb2_reduction(head[:-1], head[-1], total - tail)
-            or (
-                total % tail == 0
-                and comb2_reduction(head[:-1], head[-1], total // tail)
-            )
+            comb2(head[:-1], head[-1], total - tail)
+            or (total % tail == 0 and comb2(head[:-1], head[-1], total // tail))
             or (
                 str(total) != str(tail)
                 and str(total).endswith(str(tail))
-                and comb2_reduction(
-                    head[:-1], head[-1], int(str(total)[: -len(str(tail))])
-                )
+                and comb2(head[:-1], head[-1], int(str(total)[: -len(str(tail))]))
             )
         )
 
@@ -176,22 +74,21 @@ def part_2(file):
         total, els = line.strip().split(":")
         els = [int(e) for e in els.strip().split()]
         total = int(total)
-        valid = comb2_reduction(els[:-1], els[-1], total)
+        valid = comb2(els[:-1], els[-1], total)
         if valid:
             ans += total
-            print(f"{total} == {els}")
     return ans
 
 
 if __name__ == "__main__":
-    # with io.StringIO(EXAMPLE) as f:
-    #     print(part_1(f))
-
-    # with open(INPUT_FILE_PATH) as f:
-    #     print(part_1(f))
-
-    # with io.StringIO(EXAMPLE) as f:
-    #     print(part_2(f))  # 11387
+    with io.StringIO(EXAMPLE) as f:
+        print(f"{part_1(f)} == {PART1_EXAMPLE_OUTPUT}?")
 
     with open(INPUT_FILE_PATH) as f:
-        print(part_2(f))
+        print(f"{part_1(f)} == {PART1_EXAMPLE_OUTPUT}?")
+
+    with io.StringIO(EXAMPLE) as f:
+        print(f"{part_2(f)} == {PART1_OUTPUT}?")
+
+    with open(INPUT_FILE_PATH) as f:
+        print(f"{part_2(f)} == {PART2_OUTPUT}?")
---
+++ src/aoc_2024/test_day_07.py
@@ -0,0 +1,32 @@
+import io
+
+from aoc_2024.day_07 import (
+    EXAMPLE,
+    INPUT_FILE_PATH,
+    PART1_EXAMPLE_OUTPUT,
+    PART1_OUTPUT,
+    PART2_EXAMPLE_OUTPUT,
+    PART2_OUTPUT,
+    part_1,
+    part_2,
+)
+
+
+def test_part_1_example():
+    with io.StringIO(EXAMPLE) as f:
+        assert part_1(f) == PART1_EXAMPLE_OUTPUT
+
+
+def test_part_1_full():
+    with open(INPUT_FILE_PATH) as f:
+        assert part_1(f) == PART1_OUTPUT
+
+
+def test_part_2_example():
+    with io.StringIO(EXAMPLE) as f:
+        assert part_2(f) == PART2_EXAMPLE_OUTPUT
+
+
+def test_part_2_full():
+    with open(INPUT_FILE_PATH) as f:
+        assert part_2(f) == PART2_OUTPUT
