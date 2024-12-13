f11ae25aae16f10a0aae2b543d9efe156dc6a23a
stilyaniv <stennly.mkiv@gmail.com>
2024-12-13 10:24:00 +0000

    aoc 2024 day 12 wip

---
+++ src/aoc_2024/day_12.py
@@ -0,0 +1,219 @@
+"""
+https://adventofcode.com/
+"""  # TODO link to problem
+import io
+from pathlib import Path
+from pprint import pprint
+from typing import List
+
+EXAMPLE = """\
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+"""
+
+
+INPUT_FILE_PATH = f"{Path(__file__).parent / Path(__file__).stem}.txt"
+
+PART1_EXAMPLE_OUTPUT = None
+PART2_EXAMPLE_OUTPUT = None
+
+
+def create_grid(file):
+    grid = []
+    special_chars = {}
+    x_len, y_len = 0, 0
+    for y, line in enumerate(file):
+        y_len += 1
+        row = []
+        for x, char in enumerate(line.strip()):
+            row.append(char)
+            if char in special_chars:
+                special_chars[char] += [(x, y)]
+            else:
+                special_chars[char] = [(x, y)]
+        grid.append(row)
+
+    x_len, y_len = x + 1, y + 1
+    return grid, x_len, y_len, special_chars
+
+
+def traverse(grid, x_len, y_len, x, y, square, visited):
+    # area, perim = 0, 0
+    if not (0 <= x < x_len and 0 <= y < y_len):
+        # visited.extend([(x, y)])
+        return
+    if grid[y][x] != square:
+        # return [(x, y)], 0, 1
+        return
+    if grid[y][x] == square:
+        visited.extend([(x, y)])
+    # left = grid[y][x-1]
+    # right = grid[y][x+1]
+    # up = grid[y-1][x]
+    # down = grid[y+1][x]
+    # area += 1
+    # if ()
+        # left = traverse(grid, x_len, y_len, x - 1, y, square)
+        right = traverse(grid, x_len, y_len, x + 1, y, square, visited)
+        # up = traverse(grid, x_len, y_len, x, y - 1, square)
+        # down = traverse(grid, x_len, y_len, x, y + 1, square)
+    # loc = right[0]
+    # visited.extend(loc)
+    # area += right[1]
+    # perim += right[2]
+
+    # print(right)
+    # print((x, y))
+    # return visited, area, perim
+    return 0, 0
+
+
+def part_1(file):
+    grid, x_len, y_len, squares = create_grid(file)
+
+    perim = []
+    visited = []
+    for y in range(y_len):
+        for x in range(x_len):
+            # if (x, y) in not:
+            #     continue
+            square = grid[y][x]
+            area, perim = traverse(grid, x_len, y_len, x, y, square, visited)
+            # if (x, y) not in gardens:
+            #     gardens[(x, y)] = (area, perim)
+            #     mapped_squares.extend(visited)
+
+            # grid[y][x] = Square(x, y, grid)
+
+    pprint(visited)
+    return
+
+
+class Square:
+
+    def __init__(self, x, y, grid) -> None:
+        self.x = x
+        self.y = y
+        self.E = grid[y][x-1]
+        self.W = grid[y][x+1]
+        self.N = grid[y-1][x]
+        self.S = grid[y+1][x]
+        self.garden = None
+        # self.E = None
+        # self.W = None
+        # self.N = None
+        # self.S = None
+
+    def perim(self):
+        neighbours = [self.E, self.W, self.N, self.S]
+        return sum(1 if s else 0 for s in neighbours)
+
+    def get_neighbour_gardens(self):
+        return
+
+
+class Garden:
+    def __init__(self):
+        self.squares = []
+
+    def add(self, square):
+        self.squares.append(square)
+
+
+def part_1_objects(file):
+    # if (x, y) in not :
+    #     continue
+    # square = grid[y][x]
+    # left = grid[y][x-1]
+    # right = grid[y][x+1]
+    # up = grid[y-1][x]
+    # down = grid[y+1][x]
+
+    # visited, area, perim = traverse(grid, x_len, y_len, x, y, square)
+    # if (x, y) not in gardens:
+    #     gardens[(x, y)] = (area, perim)
+    #     mapped_squares.extend(visited)
+
+    # grid[y][x] = Square(x, y, grid)
+
+    # pprint(gardens)
+    return gardens
+
+
+def part_1_left_to_right(file):
+    """ 
+    traverse left to right top to bottom
+    doesn't work without extra changes as we create new gardens too early
+    """
+    grid, x_len, y_len, squares = create_grid(file)
+
+    # mapped_squares = []
+    squares = {}
+    gardens = {}
+    garden_index = 0
+    for y in range(y_len):
+        for x in range(x_len):
+            if (x+1, y) in gardens and grid[y][x] == grid[y][x+1]:
+                gardens[(x, y)] = gardens[(x+1, y)]
+            elif (x-1, y) in gardens and grid[y][x] == grid[y][x-1]:
+                gardens[(x, y)] = gardens[(x-1, y)]
+            elif (x, y+1) in gardens and grid[y][x] == grid[y+1][x]:
+                gardens[(x, y)] = gardens[(x, y+1)]
+            elif (x, y-1) in gardens and grid[y][x] == grid[y-1][x]:
+                gardens[(x, y)] = gardens[(x, y-1)]
+            else:
+                gardens[(x, y)] = garden_index
+                # grid[y][x] = garden_index
+                garden_index += 1
+    for (x, y), garden_index in gardens.items():
+        grid[y][x] = garden_index
+    pprint(grid)
+    new_grid = "\n".join([" ".join([f"{str(c):>2}" for c in row]) for row in grid])
+    print(new_grid)
+
+    # if (x, y) in not :
+    #     continue
+    # square = grid[y][x]
+    # left = grid[y][x-1]
+    # right = grid[y][x+1]
+    # up = grid[y-1][x]
+    # down = grid[y+1][x]
+
+    # visited, area, perim = traverse(grid, x_len, y_len, x, y, square)
+    # if (x, y) not in gardens:
+    #     gardens[(x, y)] = (area, perim)
+    #     mapped_squares.extend(visited)
+
+    # grid[y][x] = Square(x, y, grid)
+
+    # pprint(gardens)
+    return gardens
+
+
+def part_2(file):
+    for line in file:
+        pass
+
+    return
+
+
+if __name__ == "__main__":
+    with io.StringIO(EXAMPLE) as f:
+        print(f"{part_1(f)} == {PART1_EXAMPLE_OUTPUT}?")
+
+    # with open(INPUT_FILE_PATH) as f:
+    #     print(part_1(f))
+
+    # with io.StringIO(EXAMPLE) as f:
+    #     print(f"{part_2(f)} == {PART2_EXAMPLE_OUTPUT}?")
+
+    # with open(INPUT_FILE_PATH) as f:
+    #     print(part_2(f))
---
+++ src/aoc_2024/day_12_notes.md
@@ -0,0 +1,23 @@
+```
+ R  R  R  R  I  I  C  C  F  F
+ R  R  R  R  I  I  C  C  C  F
+ V  V  R  R  R  C  C  F  F  F
+ V  V  R  C  C  C  J  F  F  F
+ V  V  V  V  C  J  J  C  F  E
+ V  V  I  V  C  C  J  J  E  E
+ V  V  I  I  I  C  J  J  E  E
+ M  I  I  I  I  I  J  J  E  E
+ M  I  I  I  S  I  J  E  E  E
+ M  M  M  I  S  S  J  E  E  E
+
+ 0  0  0  0  1  1  2  2  3  3
+ 0  0  0  0  1  1  2  2  2  3
+ 4  4  0  0  0  5  5  6  6  6
+ 4  4  0  7  7  7  8  6  6  6
+ 4  4  4  4  7  9  9 10  6 11
+ 4  4 12  4  7  7  9  9 13 13
+ 4  4 12 12 12  7  9  9 13 13
+14 15 15 15 15 15  9  9 13 13
+14 15 15 15 16 15  9 17 17 17
+14 14 14 15 16 16  9 17 17 17
+```
